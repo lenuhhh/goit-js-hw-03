d4281897ba3411d82e000da157d3d14981eb852a
lenuhhh <161879235+lenuhhh@users.noreply.github.com>
2024-06-25 15:35:03 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function makeArray(firstArray, secondArray, maxLength) {
     const combinedArray = firstArray.concat(secondArray);
-    if (combinedArray.lenght > maxLength); {
+    if (combinedArray.lenght > maxLength) {
            return combinedArray.slice(0, maxLength); 
     }
     return combinedArray;
