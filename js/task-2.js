3db7c8a1037ceda8b45199272128216808fe0037
lenuhhh <161879235+lenuhhh@users.noreply.github.com>
2024-06-25 15:34:35 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,10 @@
 function makeArray(firstArray, secondArray, maxLength) {
-    combinedArray = firstArray.concat(secondArray);
+    const combinedArray = firstArray.concat(secondArray);
     if (combinedArray.lenght > maxLength); {
            return combinedArray.slice(0, maxLength); 
     }
     return combinedArray;
-}
+};
 
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
